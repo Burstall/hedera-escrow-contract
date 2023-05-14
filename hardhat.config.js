51a4aaffe954fe7545a332d023ba0c6f42dfc442
Burstall <richard@alphacenturi.com>
2023-05-14 13:17:31 +0100

    extended timeout due to testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,9 @@ require('hardhat-contract-sizer');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+	mocha: {
+		timeout: 10000000,
+	},
 	solidity: {
 		version: '0.8.18',
 		settings: {
